cb1d8c2d9a1cbf4e8821326a14271f45d2ecc5d8
icing808 <icing808@aliyun.com>
2021-04-12 12:43:02 +0800

    --modify comments

--- taskiapp/server/api.js
+++ taskiapp/server/api.js
@@ -8,12 +8,12 @@ router.post('/api/user/register',(req,res) => {
 	//Query name = req.body.name 
 	models.Login.find({name: req.body.name},(err,data) => {
 		if(err){
-			res.send({'status': 1002, 'message': '查询失败', 'data': err});
+			res.send({'status': 1002, 'message': 'Query failure', 'data': err});
 		}else{
-			console.log('查询成功'+data)
+			console.log('Query success'+data)
 			//data : A collection with the same name in the returned database
 			if(data.length > 0){
-				res.send({'status': 1001, 'message': '该用户名已经注册！'});
+				res.send({'status': 1001, 'message': 'The user name is already registered！'});
 			}else{
 				let newName = new models.Login({
                     name : req.body.name,
@@ -22,9 +22,9 @@ router.post('/api/user/register',(req,res) => {
                 //newName.save insert to table
                 newName.save((err,data) => {
                     if (err) {
-                        res.send({'status': 1002, 'message': '注册失败！', 'data': err});
+                        res.send({'status': 1002, 'message': 'Sign up failure！', 'data': err});
                     } else {
-                        res.send({'status': 1000, 'message': '注册成功!'});
+                        res.send({'status': 1000, 'message': 'Sign up successful!'});
                     }
                 });
 			}
@@ -36,13 +36,13 @@ router.post('/api/user/login',(req,res) => {
 	models.Login.find({name: req.body.name, password: req.body.password},(err,data) => {
 		if (err) {
             // res.send(err);
-            res.send({'status': 1002, 'message': '查询数据库失败!', 'data': err});
+            res.send({'status': 1002, 'message': 'Failed to query database!', 'data': err});
         } else {
             console.log(data)
             if (data.length > 0) {
-                res.send({'status': 1000, 'message': '登录成功!', 'data': data});
+                res.send({'status': 1000, 'message': 'Sign In successful!', 'data': data});
             } else {
-                res.send({'status': 1001, 'message': '登录失败，该用户没有注册!', 'data': err});
+                res.send({'status': 1001, 'message': 'Sign In failure. The user is not registered!', 'data': err});
             }
         }
 	})
@@ -60,19 +60,19 @@ router.get('/api/user/all',(req,res) => {
 //delete user
 router.post('/api/user/delete',(req,res) => {
     models.Login.remove({name: req.body.name},(err,data) => {
-        if (err) {
-            //res.send(err);
-            res.send({'status': 1004, 'message': '删除失败!', 'data': err});
-        } else {
-        	res.send({'status': 1003, 'message': '删除成功!', 'data': data});
+        // if (err) {
+        //     //res.send(err);
+        //     res.send({'status': 1004, 'message': 'Delete failure!', 'data': err});
+        // } else {
+        // 	res.send({'status': 1003, 'message': 'Delete successful!', 'data': data});
 
-        }
+        // }
     });
     models.Login.find((err,data) => {
         if (err) {
             console.log(err)
         } else {
-            res.send({'status': 1000, 'message': '更新成功！', 'data': data});
+            res.send({'status': 1000, 'message': 'Update successful！', 'data': data});
         }
     });
 });
