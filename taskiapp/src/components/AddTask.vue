a64c07c875fa910f420453e03459f517ee9d9bda
Zhouyu Huang <zhouyu.huang@mail.utoronto.ca>
2021-04-13 01:34:52 +0800

    Added sample todos

--- taskiapp/src/components/AddTask.vue
+++ taskiapp/src/components/AddTask.vue
@@ -1,9 +1,52 @@
 <template>
-    <div>Add Task</div>
+    <div>
+        <div>Add Task</div>
+        <div>
+            <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+        </div>
+    </div>
 </template>
 <script>
+import axios from 'axios';
+import Todos from '../components/Todos';
+export default {
+  name: 'Home',
+  components: {
+    Todos
+  },
+  data() {
+    return {
+      todos: []
+    }
+  },
 
+  // Used API for todo list JSON server
+  // Reference: https://jsonplaceholder.typicode.com/
+  
+  methods: {
+    addTodo(newTodo) {
+      const { title, completed } = newTodo;
+      axios.post('https://jsonplaceholder.typicode.com/todos', {
+        title,
+        completed
+      })
+        .then(res => this.todos = [...this.todos, res.data])
+        .catch(err => console.log(err));
+    },
+
+    deleteTodo(id) {
+      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(this.todos = this.todos.filter(todo => todo.id !== id))
+        .catch(err => console.log(err));
+    },
+  },
+  created() {
+    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=15')
+      .then(res => this.todos = res.data)
+      .catch(err => console.log(err));
+  }
+}
 </script>
 <style scoped>
-
+    
 </style>
---
+++ taskiapp/src/components/TodoItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="todo-item">
+    <div>
+      {{todo.title}}
+      <button @click="$emit('del-todo', todo.id)" class="del">
+        x
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: ["todo"],
+}
+</script>
+
+<style scoped>
+  .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+  }
+
+  .del{
+    float: right;
+    width: 30px;
+    height: 30px;
+    margin-bottom: 10px;
+  }
+</style>
+
