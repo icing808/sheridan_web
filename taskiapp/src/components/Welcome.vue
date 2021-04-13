99c716fb48ee951d3cefc007dc21c657c1d3d444
YogaJi <75383370+YogaJi@users.noreply.github.com>
2021-04-13 17:10:20 +0800

    Update Welcome.vue
    Add style at the bottom, and add id,class in the html

--- taskiapp/src/components/Welcome.vue
+++ taskiapp/src/components/Welcome.vue
@@ -3,14 +3,14 @@
     <img src="../assets/welcome-logo.png" class="logo">
     <div>
       <img src="../assets/welcome-pic.png">
-      <h1>Hello!</h1>
-      <p>Lorem ipsum dolor sit amet, consect adiposing elit, sed do eiusmod temttingcig ut laborer et dolore magna al.</p>
+      <h1 id="hello">Hello!</h1>
+      <p id="intro">Lorem ipsum dolor sit amet, consect adiposing elit, sed do eiusmod temttingcig ut laborer et dolore magna al.</p>
       <div class="welcome-buttons">
-        <b-button>
-          <router-link to="/Login">LOGIN</router-link>
-        </b-button>
-        <b-button>
-          <router-link to="/SignUp">SIGN UP</router-link>
+        <b-button class="btn log">
+          <router-link to="/Login" class="p1">LOGIN</router-link>
+      </b-button><br><br>
+        <b-button class="btn snup">
+          <router-link to="/SignUp" class="p2">SIGN UP</router-link>
         </b-button>
       </div>
     </div>
@@ -31,5 +31,38 @@ export default {
 <style scoped>
 .welcome-buttons{
 
+}
+.btn{
+    width: 280px;
+    height: 40px;
+    border:none;
+}
+.log{
+    background:linear-gradient(to right, #04B4EE,#1ED9D3);
+    color:white;
+}
+.snup{
+    border:2px solid transparent;
+    border-image:linear-gradient(to right, #04B4EE,#1ED9D3);
+    border-image-slice:1;
+    background-color:white;
+    border-radius:8px;
+}
+.p1{
+    color:white
+}
+.p2{
+    color:black
+}
+#intro{
+    margin-left:16%;
+    margin-right:13%;
+    text-align:left;
+    color:#6c757d;
+    font-size:14px;
+    margin-bottom:4%;
+}
+#hello{
+    margin-top:6%;
 }
 </style>
