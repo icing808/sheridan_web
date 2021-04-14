8eabfa9733011a6ca1081d741ce43fb28e5a3e35
Zhouyu Huang <zhouyu.huang@mail.utoronto.ca>
2021-04-14 17:48:53 +0800

    Update TaskList.vue partially

--- taskiapp/src/components/TaskList.vue
+++ taskiapp/src/components/TaskList.vue
@@ -1,6 +1,125 @@
 <template>
     <div>
-        <h1>Task List</h1>
+        <div>
+            <b-card no-body>
+                <b-tabs pills card>
+                <b-tab title="All" active @click="getAll(-1)">
+                    <b-card-text>
+                        <input type="hidden" v-model="q_status">
+                        <div v-show="showAll">
+                            <p v-for="item in array" :key="item._id">
+                                
+                                    <b-form-checkbox
+                                        id="checkbox-1"
+                                        v-model="status"
+                                        name="checkbox-1"
+                                        value="accepted"
+                                        unchecked-value="not_accepted"
+                                    >
+                                    I accept the terms and use
+                                    </b-form-checkbox>
+                                
+                                <ul>
+                                    <li>UserId: {{item.user_id}}</li>
+                                    <li v-if="1 === item.category_id" >Category: Study</li>
+                                    <li v-else-if="2 === item.category_id" >Category: Work</li>
+                                    <li v-else-if="3 === item.category_id" >Category: Other</li>
+                                    <li v-else>Category: No Category</li>
+
+                                    <li v-if="1 === item.level" >Level: General</li>
+                                    <li v-else-if="2 === item.level" >Level: Important</li>
+                                    <li v-else-if="3 === item.level" >Level: Emergent</li>
+                                    <li v-else>Level: No Level</li>
+
+                                    <li>Title: {{item.title}}</li>
+                                    <li>Content: {{item.content}}</li>
+                                    <li>Start Time: {{item.start_time}}</li>
+                                    <li>End Time: {{item.end_time}}</li>
+                                    <li v-if="0 === item.status" >Status: Pending</li>
+                                    <li v-else-if="1 === item.status" >Status: On going</li>
+                                    <li v-else-if="2 === item.status" >Status: Finish</li>
+                                    <li v-else-if="3 === item.status" >Status: Ignore</li>
+                                    <li v-else-if="4 === item.status" >Status: Expire</li>
+                                    <li v-else>Status: No Status</li>
+                                    <li v-show="delBtnShow"><button @click="deleteone(item._id)">Delete</button></li>
+                                    <li v-show="doneBtnShow"><button @click="updateStatus(item._id)">Done</button></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </b-card-text>
+                </b-tab>
+                <b-tab title="Ongoing" @click="getAll(1)">
+                    <b-card-text>
+                        <input type="hidden" v-model="q_status">
+                        <div v-show="showAll">
+                            <p v-for="item in array" :key="item._id">
+                                <ul>
+                                    <li>UserId: {{item.user_id}}</li>
+                                    <li v-if="1 === item.category_id" >Category: Study</li>
+                                    <li v-else-if="2 === item.category_id" >Category: Work</li>
+                                    <li v-else-if="3 === item.category_id" >Category: Other</li>
+                                    <li v-else>Category: No Category</li>
+
+                                    <li v-if="1 === item.level" >Level: General</li>
+                                    <li v-else-if="2 === item.level" >Level: Important</li>
+                                    <li v-else-if="3 === item.level" >Level: Emergent</li>
+                                    <li v-else>Level: No Level</li>
+
+                                    <li>Title: {{item.title}}</li>
+                                    <li>Content: {{item.content}}</li>
+                                    <li>Start Time: {{item.start_time}}</li>
+                                    <li>End Time: {{item.end_time}}</li>
+                                    <li v-if="0 === item.status" >Status: Pending</li>
+                                    <li v-else-if="1 === item.status" >Status: On going</li>
+                                    <li v-else-if="2 === item.status" >Status: Finish</li>
+                                    <li v-else-if="3 === item.status" >Status: Ignore</li>
+                                    <li v-else-if="4 === item.status" >Status: Expire</li>
+                                    <li v-else>Status: No Status</li>
+                                    <li v-show="delBtnShow"><button @click="deleteone(item._id)">Delete</button></li>
+                                    <li v-show="doneBtnShow"><button @click="updateStatus(item._id)">Done</button></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </b-card-text>
+                </b-tab>
+                <b-tab title="Completed" @click="getAll(2)">
+                    <b-card-text>
+                        <input type="hidden" v-model="q_status">
+                        <div v-show="showAll">
+                            <p v-for="item in array" :key="item._id">
+                                <ul>
+                                    <li>UserId: {{item.user_id}}</li>
+                                    <li v-if="1 === item.category_id" >Category: Study</li>
+                                    <li v-else-if="2 === item.category_id" >Category: Work</li>
+                                    <li v-else-if="3 === item.category_id" >Category: Other</li>
+                                    <li v-else>Category: No Category</li>
+
+                                    <li v-if="1 === item.level" >Level: General</li>
+                                    <li v-else-if="2 === item.level" >Level: Important</li>
+                                    <li v-else-if="3 === item.level" >Level: Emergent</li>
+                                    <li v-else>Level: No Level</li>
+
+                                    <li>Title: {{item.title}}</li>
+                                    <li>Content: {{item.content}}</li>
+                                    <li>Start Time: {{item.start_time}}</li>
+                                    <li>End Time: {{item.end_time}}</li>
+                                    <li v-if="0 === item.status" >Status: Pending</li>
+                                    <li v-else-if="1 === item.status" >Status: On going</li>
+                                    <li v-else-if="2 === item.status" >Status: Finish</li>
+                                    <li v-else-if="3 === item.status" >Status: Ignore</li>
+                                    <li v-else-if="4 === item.status" >Status: Expire</li>
+                                    <li v-else>Status: No Status</li>
+                                    <li v-show="delBtnShow"><button @click="deleteone(item._id)">Delete</button></li>
+                                    <li v-show="doneBtnShow"><button @click="updateStatus(item._id)">Done</button></li>
+                                </ul>
+                            </p>
+                        </div>
+                    </b-card-text>
+                </b-tab>
+                </b-tabs>
+            </b-card>
+        </div>
+        <!-- <h1>Task List</h1>
         <button @click="getAll(-1)">Get All Task</button>
         <button @click="getAll(0)">Get Pending Task</button>
         <button @click="getAll(1)">Get On going Task</button>
@@ -35,7 +154,7 @@
                 </ul>
             </p>
         </div>
-        <div>{{msg}}</div>
+        <div>{{msg}}</div> -->
         
         <p><router-link to="/AddTask2">Add Task</router-link></p>
         <p><router-link to="/Home">Home</router-link></p>
@@ -52,7 +171,8 @@ export default {
             showAll: false,
             delBtnShow: true,
             doneBtnShow: true,
-            q_status: ''
+            q_status: '',
+            status: 'not_accepted'
         }
     },
     methods:{
